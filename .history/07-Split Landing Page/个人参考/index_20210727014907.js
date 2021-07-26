1f6e87cd1fed4eef31eed554208081fd6d10d88a
maxwelllong <longxq_work@163.com>
2021-07-27 02:03:50 +0800

    07 finished

---
+++ .history/07-Split Landing Page/个人参考/index_20210727014907.js
@@ -0,0 +1,8 @@
+const left = document.querySelector('.left')
+const right = document.querySelector('.right')
+const container = document.querySelector('.container')
+
+left.addEventListener('mouseenter',function(){
+    // console.log(left.classList);
+    left.classList.add('')
+})
---
+++ .history/07-Split Landing Page/个人参考/index_20210727015109.js
@@ -0,0 +1,9 @@
+const left = document.querySelector('.left')
+const right = document.querySelector('.right')
+const container = document.querySelector('.container')
+
+left.addEventListener('mouseenter',function(){
+    // console.log(left.classList);
+    left.classList.add('.hover-left')
+    
+})
